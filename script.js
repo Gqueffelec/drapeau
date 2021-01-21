628c7cc4098145d3b4daa312e95ddf868533d054
Guillaume QUEFFELEC <queffelec.guillaume@gmail.com>
2021-01-21 18:08:33 +0100

    mise en forme sass et couleur de base sauf Tchequie

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    // $(".drapeau").hide();
+    $("#france").show();
+    initialColor("france", "blue");
+    initialColor("belgique", "yellow");
+    initialColor("allemagne", "red");
+    initialColor("hollande", "white");
+    initialColor("pologne", "red");
+});
+
+
+function initialColor(flagDiv, color) {
+    $("#" + flagDiv).children().filter("div").children().each(function () {
+        $(this).addClass(color);
+    });
+}
