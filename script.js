58dfeb388da616e6aa93f166abde57c8484baba3
Guillaume QUEFFELEC <queffelec.guillaume@gmail.com>
2021-01-22 08:30:54 +0100

    ajout du score board avec niveau,timer et clics + valider apparait uniquement avec la bonne solution

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var allFlagId = ["france", "belgique", "allemagne", "hollande", "pologne", "tcheque"];
-
 var actualFlagId = allFlagId[0];
 var flagIndex = 0;
-
-var bonneSolution = false;
+var numberOfClic = 0;
+var start = 0;
+var end = 0;
+var diff = 0;
+var timer = 0;
 
 $(document).ready(function () {
     nextFlag(flagIndex);
@@ -13,6 +15,11 @@ $(document).ready(function () {
     initialColor("hollande", "white");
     initialColor("pologne", "red");
     initialColor("tcheque", "red");
+    updateScore();
+    start = new Date();
+    chrono();
+    updateClic();
+    $('#valider').hide();
 });
 $(function () {
     switchColor("france", "blue", "red", "white");
@@ -25,6 +32,79 @@ $(function () {
 
 $(function () {
     $('#valider').click(function () {
+        let soluce;
+        switch (flagIndex) {
+            case 0:
+                soluce = "bluewhitered";
+                checkSolution(soluce);
+                updateScore();
+                break;
+            case 1:
+                soluce = "blackyellowred";
+                checkSolution(soluce);
+                updateScore();
+                break;
+            case 2:
+                soluce = "blackredyellow";
+                checkSolution(soluce);
+                updateScore();
+                break;
+            case 3:
+                soluce = "redwhiteblue";
+                checkSolution(soluce);
+                updateScore();
+                break;
+            case 4:
+                soluce = "whitered";
+                checkSolution(soluce);
+                updateScore();
+                break;
+            case 5:
+                soluce = "whiteredbluered";
+                checkSolution(soluce);
+                updateScore();
+                break;
+            default:
+                break;
+        }
+    })
+})
+
+$(function () {
+    $("#valider").click(function () {
+        flagIndex++;
+        nextFlag(flagIndex);
+        updateScore()
+        $(this).hide();
+    })
+})
+
+function initialColor(flagDiv, color) {
+    $("#" + flagDiv).children().filter("div").children().each(function () {
+        $(this).addClass(color);
+    });
+}
+
+function switchColor(flagDiv, color1, color2, color3) {
+
+    $("#" + flagDiv).children().filter("div").children().click(function () {
+        numberOfClic++;
+        updateClic();
+        let color = $(this).attr("class");
+        if (color3 == null) {
+            color3 = color1;
+        }
+        switch (color) {
+            case color1:
+                $(this).removeClass(color1).addClass(color2);
+                break;
+            case color2:
+                $(this).removeClass(color2).addClass(color3);
+                break;
+            case color3:
+                $(this).removeClass(color3).addClass(color1);
+                break;
+        }
         let soluce;
         switch (flagIndex) {
             case 0:
@@ -55,32 +135,6 @@ $(function () {
                 break;
         }
     })
-})
-
-function initialColor(flagDiv, color) {
-    $("#" + flagDiv).children().filter("div").children().each(function () {
-        $(this).addClass(color);
-    });
-}
-
-function switchColor(flagDiv, color1, color2, color3) {
-    $("#" + flagDiv).children().filter("div").children().click(function () {
-        let color = $(this).attr("class");
-        if (color3 == null) {
-            color3 = color1;
-        }
-        switch (color) {
-            case color1:
-                $(this).removeClass(color1).addClass(color2);
-                break;
-            case color2:
-                $(this).removeClass(color2).addClass(color3);
-                break;
-            case color3:
-                $(this).removeClass(color3).addClass(color1);
-                break;
-        }
-    })
 }
 
 function nextFlag(index) {
@@ -94,12 +148,34 @@ function checkSolution(soluce) {
     $("#" + allFlagId[flagIndex]).children().last().children().each(function () {
         reponse += $(this).attr('class');
     })
-    console.log(reponse)
     if (soluce == reponse) {
-        alert("FUCK YEAH");
-        flagIndex++;
-        nextFlag(flagIndex);
+        $("#valider").show();
+    }
+}
+
+function updateScore() {
+    $('#level').html("Niveau " + flagIndex + "/6 !");
+}
+
+function updateClic() {
+    $('#clic').html("Nombre de clics : " + numberOfClic);
+}
+
+function chrono() {
+    end = new Date();
+    diff = end - start;
+    diff = new Date(diff);
+    var sec = diff.getSeconds();
+    var min = diff.getMinutes();
+    var hr = diff.getHours() - 1;
+    $('#time').html(addZero(hr) + ":" + addZero(min) + ":" + addZero(sec));
+    timer = setTimeout("chrono()", 1000);
+}
+
+function addZero(nombre) {
+    if (nombre < 10) {
+        return "0" + nombre;
     } else {
-        alert("L like a LOSER")
+        return nombre;
     }
 }
